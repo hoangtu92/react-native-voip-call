994093fdd30ed6d261f59676839b24e7bca0da87
ML <hoangtu92@live.com>
2021-05-03 22:45:05 +0800

    update

--- android/src/main/java/com/ajith/voipcall/RNVoipNotificationHelper.java
+++ android/src/main/java/com/ajith/voipcall/RNVoipNotificationHelper.java
@@ -8,8 +8,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 
-import android.os.PowerManager;
-import android.view.WindowManager;
+
 import androidx.core.app.NotificationCompat;
 import android.content.Context;
 import android.media.AudioAttributes;
